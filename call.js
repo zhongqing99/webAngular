3d9834a4cadc9c5af462123f94ebfc553d5416ff
zhongqing99 <772545699@qq.cpm>
2018-11-22 14:25:14 +0800

    过滤器 以及双向绑定

--- call.js
+++ call.js
@@ -1,3 +1,30 @@
 /**
  * Created by Administrator on 2018-11-21.
  */
+function call(){
+
+}
+var obj={name:'hahah'};
+function say(){
+    console.log(this.name);
+}
+
+console.log(Function.prototype.call());
+say();
+
+
+//实现call方法
+
+Function.prototype.myCall=function(thisObj){
+   var source=this.toString();
+    source.replace(/this/,function(resoult){
+        return arguments[0]
+    });
+
+    //var func=new Function('thisObj',source);
+    //(source)(thisObj);
+    console.log(source);
+    eval('('+source+')('+thisObj+')');
+};
+
+say.myCall(obj);
--- injector.js
+++ injector.js
@@ -1,4 +1,31 @@
 /**
  * Created by Administrator on 2018-11-21.
  */
+//详解依赖注入
+var greet=function(words,school){
+    console.log(words.text,school.text);
+};
+//就相当于我们的对象库 里面存放着所有能够注入的对象或者服务；
+var registry={
+    words:{
+        text:'hello'
+    },
+    school:{
+        text:'woshixiaoming'
+    }
+};
+//自动注入服务对象并且运行参数里面的函数
+var inject=function(func){
+    var source=func.toString();
+    var matchers=source.match(/function\s?\((\w+),\)/);
+    var argnames=matchers.slice(1);
+    var args=[];
+    for(var i=0;i<argnames.length;i++){
+        var argObj=registry[argnames[i]];
+        args.push(argObj);
+    }
+    func.apply(null,args);
 
+    //func(registry.words);
+};
+inject(greet);//输出hello
